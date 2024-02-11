8b20845521933183df3f1f2a518d38f40eab8455
abuabdullah23 <abuobaidazayed@gmail.com>
2024-02-11 11:57:33 +0600

    Delete task by id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -13,12 +13,17 @@ export class TasksController {
     }
 
     @Get('/:id')
-    getTaskById(@Param('id') id: string):Task {
+    getTaskById(@Param('id') id: string): Task {
         return this.taskService.getTaskById(id);
     }
 
     @Post()
     createTask(@Body() createTaskDto: CreateTaskDto): Task {
         return this.taskService.createTask(createTaskDto);
+    }
+
+    @Delete('/:id')
+    deleteTaskById(@Param('id') id: string): void {
+        this.taskService.deleteTaskById(id);
     }
 }
