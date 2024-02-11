1e3e43d58030e4cd3ca61fe25ea97616f922dc7e
abuabdullah23 <abuobaidazayed@gmail.com>
2024-02-11 15:51:48 +0600

    Add filter by status and search method

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -11,6 +12,23 @@ export class TasksService {
 
     getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    // filter functionality
+    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+        console.log(filterDto);
+        const { status, search } = filterDto;
+        let tasks = this.getAllTasks();
+
+        if (status) {
+            tasks = tasks.filter(task => task.status === status)
+        }
+
+        if(search){
+            tasks = tasks.filter(task => task.title.includes(search) || task.description.includes(search))
+        }
+
+        return tasks;
     }
 
     getTaskById(id: string): Task {
