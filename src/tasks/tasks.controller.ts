1eaa35550866634edc27c97e2df92b0d7174c80c
abuabdullah23 <abuobaidazayed@gmail.com>
2024-02-13 21:35:02 +0600

    ValidationPipe for filter tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,13 +3,14 @@ import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private taskService: TasksService) { }
 
     @Get()
-    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] { // filter functionality
+    getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] { // filter functionality
         // console.log(filterDto);
 
         if(Object.keys(filterDto).length){
@@ -39,7 +40,7 @@ export class TasksController {
     @Patch('/:id/status')
     updateTaskStatus(
         @Param('id') id: string,
-        @Body('status') status: TaskStatus): Task {
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus): Task {
         // console.log(id, status);
         return this.taskService.updateTaskStatus(id, status)
     }
